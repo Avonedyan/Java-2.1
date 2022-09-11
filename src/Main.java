f643dfa839060e87664afd618ea5cb377d8961fa
Polina Naydenova <avonedyan@gmail.com>
2022-09-11 22:15:54 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 20000; // стоимость билета
+        int bonus = 20; // количество рублей для одной бонусной мили
+        System.out.println("Количество бонусных миль:");
+        System.out.println(ticketprice / bonus);
+    }
+}

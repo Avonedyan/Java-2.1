d38586373bb53e2214d69edb9f222a30eaa4d7e8
Avonedyan <106354816+Avonedyan@users.noreply.github.com>
2022-09-15 04:17:37 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketprice = 20000; // стоимость билета
+        int ticketPrice = 20000; // стоимость билета
         int bonus = 20; // количество рублей для одной бонусной мили
         System.out.println("Количество бонусных миль:");
         System.out.println(ticketprice / bonus);
